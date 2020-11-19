3906c6f1448ef699c0787af1813cbc7e76b99a30
Matheus Rigote <matheus.rigote@gmail.com>
2020-11-19 01:05:26 -0300

    Config landingpage

---
+++ app/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'f6128edb75f501d7684afaec6752fd73'),
+    },
+  },
+});
---
+++ app/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '37b03b02-f5b8-433e-9a64-cfc969604990'
+};
